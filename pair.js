c015ea0a35cc55b91b0135da67c65902b64c6139
Tylor K9 <spartankelvin6@gmail.com>
2025-05-31 18:34:48 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -8,17 +8,17 @@ const MESSAGE = process.env.MESSAGE || `
 *SESSION GENERATED SUCCESSFULY* ✅
 
 *Gɪᴠᴇ ᴀ ꜱᴛᴀʀ ᴛᴏ ʀᴇᴘᴏ ꜰᴏʀ ᴄᴏᴜʀᴀɢᴇ* 🌟
-https://github.com/GuhailTechInfo/ULTRA-MD
+https://github.com/Tylorkix/Ben10
 
 *Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ ꜰᴏʀ ϙᴜᴇʀʏ* 💭
 https://t.me/GlobalBotInc
-https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07
+https://whatsapp.com/channel/0029VaYpDLx4tRrrrXsOvZ3U
 
 
 *Yᴏᴜ-ᴛᴜʙᴇ ᴛᴜᴛᴏʀɪᴀʟꜱ* 🪄 
-https://youtube.com/GlobalTechInfo
+https://www.youtube.com/@Tylor-K9-ke
 
-*ULTRA-MD--WHATTSAPP-BOT* 🥀
+*BENTEN-MD--WHATTSAPP-BOT* 🥀
 `;
 
 const { upload } = require('./mega');
